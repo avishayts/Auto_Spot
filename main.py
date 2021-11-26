1ee43a13529f7c8da2c9228aad15c4b138164fb8
Avishay <avishaytsur1@gmail.com>
2021-11-26 18:00:07 +0200

    i

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 refPt = []
 cropping = False
 
@@ -11,6 +12,13 @@ def rotate(img, angle, rotPoint=None):
     rotMat = cv2.getRotationMatrix2D(rotPoint, angle, 1.0)
     dimensions = (width, height)
     return cv2.warpAffine(img, rotMat, dimensions)
+
+
+def resize(img, scalar):
+    (height, width) = img.shape[:2]
+    print(height, width)
+    return cv2.resize(img, (math.floor(width*scalar), math.floor(height*scalar)), interpolation=cv2.INTER_CUBIC)
+
 
 def click_and_crop(event, x, y, flags, param):
     # grab references to the global variables
@@ -28,15 +36,16 @@ def click_and_crop(event, x, y, flags, param):
         refPt.append((x, y))
         cropping = False
         # draw a rectangle around the region of interest
-        print(refPt)
         cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
         cv2.imshow("image", image)
 
 # def detect_apprarances
 
-path = "test.png"
-original_image = cv2.imread(path)
-image = cv2.resize(original_image, (700, 700))
+
+path = "test2.jpg"
+# original_image = cv2.imread(path)
+# image = cv2.resize(original_image, (700, 700))
+image = cv2.imread(path)
 clone = image.copy()
 cv2.namedWindow("image")
 cv2.setMouseCallback("image", click_and_crop)
@@ -53,41 +62,21 @@ while True:
 # if there are two reference points, then crop the region of interest
 # from the image and display it
 roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
-rotated = rotate(roi, -25, None)
-cv2.imshow("Rotated", rotated)
+# rotated = rotate(roi, -25, None)
+# cv2.imshow("Rotated", rotated)
+# cv2.waitKey(0)
+resized = resize(roi, 1.4)
+cv2.imshow("Resized", resized)
 cv2.waitKey(0)
 if len(refPt) == 2:
-    r, w, h = roi.shape[::-1]
-
+    h, w = roi.shape[:2]
     res = cv2.matchTemplate(clone, roi, cv2.TM_CCOEFF_NORMED)
-    threshold = .8
+    threshold = 0.8
     loc = np.where(res >= threshold)
     for pt in zip(*loc[::-1]):  # Switch collumns and rows
-        print(pt)
         cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
     cv2.imshow("result", image)
     cv2.waitKey(0)
-    # w, h, r = roi.shape[::-1]
-    # methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-    #            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
-    # for meth in methods:
-    #     img = clone.copy()
-    #     method = eval(meth)
-    #     # Apply template Matching
-    #     res = cv2.matchTemplate(img, roi, method)
-    #     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    #     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    #     if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-    #         top_left = min_loc
-    #     else:
-    #         top_left = max_loc
-    #     bottom_right = (top_left[0] + w, top_left[1] + h)
-    #     cv2.rectangle(img, top_left, bottom_right, 255, 2)
-    #     cv2.imshow("img", img)
-    #     cv2.waitKey(0)
-    # cv2.imshow("ROI", roi)
-    # cv2.waitKey(0)
 # close all open windows
 cv2.destroyAllWindows()
 
